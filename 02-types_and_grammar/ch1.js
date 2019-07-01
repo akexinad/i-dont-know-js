a2d7db1e742e289b35a504f8a961065dd614fca1
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-07-01 16:00:43 +1000

    types and grammar

---
+++ 02-types_and_grammar/ch1.js
@@ -0,0 +1,23 @@
+/*
+
+SEVEN BUILT-IN TYPES IN JAVASCRIPT
+
+- null
+- undefined
+- boolean
+- number
+- string
+- object
+- symbol
+
+All of these types excpect object are called primitives.
+
+
+Functions and Arrays are subtypes to object.
+
+VALUES HAVE TYPES, NOT VARIABLES.
+
+Note: Many developers believe there should never be any variables in the global namespace, and that everything should be contained in modules and private/separate namespaces. This is great in theory but nearly impossible in practicality; still it's a good goal to strive toward! Fortunately, ES6 added first-class support for modules, which will eventually make that much more practical.
+
+*/
+
